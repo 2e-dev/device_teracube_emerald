6f0f602cfac0144c27782c27a4a3adf0fe85aa82
Gagan Malvi <malvigagan@gmail.com>
2021-12-31 12:28:33 +0100

    emerald: Build display packages.
    Change-Id: Iaed1192fbe062d556ce0e327c4211aa94f937b7d
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- emerald.mk
+++ emerald.mk
@@ -56,3 +56,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    libdrm.vendor \
+    libvulkan
