e9f5e7b67e66ebba9c39f94fce5dba0ff2935bad
Gagan Malvi <malvigagan@gmail.com>
2021-12-31 12:13:18 +0100

    emerald: Inherit 4GB Dalvik configs.
    Change-Id: Iad2f64da560ea2d8b1c630a3583d24d40e9a607e
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- emerald.mk
+++ emerald.mk
@@ -19,3 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
